cf2a62a2204b11d3fb07d1e64443a5ee9efa65d5
alageev <alageev01@gmail.com>
2019-11-06 11:53:21 +0300

    The cake is a lie.

--- src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
@@ -1,10 +1,33 @@
 package com.efimchick.ifmo.web.jdbc;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.*;
+import java.time.LocalDate;
 
 public class RowMapperFactory {
-
     public RowMapper<Employee> employeeRowMapper() {
-        throw new UnsupportedOperationException();
+        return resultSet -> {
+            try {
+                Employee emp = new Employee(
+                        new BigInteger(String.valueOf(resultSet.getInt("ID"))),
+                        new FullName(
+                                resultSet.getString("FIRSTNAME"),
+                                resultSet.getString("MIDDLENAME"),
+                                resultSet.getString("LASTNAME")
+                        ),
+                        Position.valueOf(resultSet.getString("POSITION")),
+                        LocalDate.parse(resultSet.getString("HIREDATE")),
+                        new BigDecimal(resultSet.getInt("SALARY"))
+                );
+                return emp;
+            } catch (SQLException e) {
+                return null;
+            }
+        };
     }
 }
